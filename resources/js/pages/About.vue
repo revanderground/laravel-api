8863a0f58142f4b9009335894c93468361fe7cbc
vtnboolean <vnthuanx@gmail.com>
2022-10-02 21:56:26 +0200

    API Tag and User Controller done

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
